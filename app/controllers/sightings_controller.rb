d73390fb3ee6d5a044563133cac36bc4b0a0224c
tylergreason <tyler.greason@protonmail.com>
2020-02-24 09:09:13 -0500

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,15 @@
+class SightingsController < ApplicationController
+    def index 
+        sightings = Sighting.all 
+        render json: sightings, include: [:bird, :location]
+    end
+
+    def show 
+        sighting = Sighting.find_by(id: params[:id]) 
+        if sighting 
+            render json: {id: sighting.id, bird: sighting.bird, location: sighting.location} 
+        else 
+            render json: {message: "there was an error! "}
+        end
+    end
+end
