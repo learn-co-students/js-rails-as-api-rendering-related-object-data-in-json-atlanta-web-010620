08c5f8d169c8a6a6bff003a2a41ecba3c15e2875
tylergreason <tyler.greason@protonmail.com>
2020-02-25 13:47:38 -0500

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,13 +1,13 @@
 class SightingsController < ApplicationController
     def index 
         sightings = Sighting.all 
-        render json: sightings, include: [:bird, :location]
+        render json: SightingSerializer.new(sightings).to_serialized_json
     end
 
     def show 
         sighting = Sighting.find_by(id: params[:id]) 
         if sighting 
-            render json: {id: sighting.id, bird: sighting.bird, location: sighting.location} 
+            render json: SightingSerializer.new(sighting).to_serialized_json
         else 
             render json: {message: "there was an error! "}
         end
